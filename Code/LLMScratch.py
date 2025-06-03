9c892176d5e5d0181f3e8cd0b308344722e6e519
AaronConnolly <c22410766@mytudublin.ie>
2025-06-03 14:47:42 +0100

    started with GPT-2 code

--- Code/LLMScratch.py
+++ Code/LLMScratch.py
@@ -1,19 +1,5 @@
-class SimpleTokenizerV1:
-    def __init__(self, vocab):
-        self.str_to_int = vocab
-        self.int_to_str = {i:s for s, i in vocab.items()}
+from dataclasses import dataclass
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
 
-    def encode(self, text):
-        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
-
-        preprocessed = [
-            item.strip() for item in preprocessed if item.strip()
-        ]
-        ids = [self.str_to_int[s] for s in preprocessed]
-        return ids
-    
-    def decode(self, ids):
-        text = " ".join([self.int_to_str[i] for i in ids])
-        # Replace spaces before the specified puunctuations
-        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
-        return text
